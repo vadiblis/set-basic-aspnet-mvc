9f4dbf54ecb892c2a1b98b3e186f7652aab0f25e
Mehmet Sabancıoğlu <mehmet.sabancioglu@gmail.com>
2014-01-08 16:31:14 +0200

    Revert "ef works"
    This reverts commit 388810adbadcae5834d515a0a69afb25f314e808.

--- src/client/set-basic-aspnet-mvc/Domain/Repositories/Repository.cs
+++ src/client/set-basic-aspnet-mvc/Domain/Repositories/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Data.Entity;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 
 using set_basic_aspnet_mvc.Domain.Entities;
 
@@ -8,85 +9,54 @@ namespace set_basic_aspnet_mvc.Domain.Repositories
 {
     public class Repository<T> : IRepository<T> where T : BaseEntity
     {
-        protected SetDbContext Context;
-
-        public Repository()
-        {
-            Context = new SetDbContext();
-        }
-
         public T Create(T entity)
         {
-            return Context.Set<T>().Add(entity);
+            throw new NotImplementedException();
         }
 
         public T Update(T entity)
         {
-            Context.Entry(entity).State = EntityState.Modified;
-            return entity;
+            throw new NotImplementedException();
         }
 
         public void SoftDelete(long id, int deletedBy)
         {
-            var entity = Context.Set<T>().Find(id);
-            entity.DeletedAt = DateTime.Now;
-            entity.DeletedBy = deletedBy;
-            entity.IsDeleted = true;
+            throw new NotImplementedException();
         }
 
         public void SoftDelete(System.Linq.Expressions.Expression<Func<T, bool>> where, int deletedBy)
         {
-            var objects = Context.Set<T>().Where(where).AsEnumerable();
-            foreach (var item in objects)
-            {
-                item.DeletedAt = DateTime.Now;
-                item.DeletedBy = deletedBy;
-                item.IsDeleted = true;
-            }
+            throw new NotImplementedException();
         }
 
         public void Delete(long id)
         {
-            var entity = Context.Set<T>().Find(id);
-            Context.Set<T>().Remove(entity);
+            throw new NotImplementedException();
         }
 
         public void Delete(System.Linq.Expressions.Expression<Func<T, bool>> where)
         {
-            var objects = Context.Set<T>().Where(where).AsEnumerable();
-            foreach (var item in objects)
-            {
-                Context.Set<T>().Remove(item);
-            }
+            throw new NotImplementedException();
         }
 
         public T FindOne(System.Linq.Expressions.Expression<Func<T, bool>> where = null, params System.Linq.Expressions.Expression<Func<T, object>>[] includeProperties)
         {
-            return FindAll(where, includeProperties).FirstOrDefault();
+            throw new NotImplementedException();
         }
 
         public IQueryable<T> FindAll(System.Linq.Expressions.Expression<Func<T, bool>> where = null, params System.Linq.Expressions.Expression<Func<T, object>>[] includeProperties)
         {
-            var items = where != null
-                ? Context.Set<T>().Where(s => !s.IsDeleted).Where(where)
-                : Context.Set<T>().Where(s => !s.IsDeleted);
-
-            foreach (var property in includeProperties)
-            {
-                items.Include(property);
-            }
-
-            return items;
+            throw new NotImplementedException();
         }
 
         public IQueryable AsQueryable()
         {
-            return Context.Set<T>();
+            throw new NotImplementedException();
         }
 
-        public virtual bool SaveChanges()
+        public bool SaveChanges()
         {
-            return 0 < Context.SaveChanges();
+            throw new NotImplementedException();
         }
     }
 }
