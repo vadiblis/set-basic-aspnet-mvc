ec2eb1f5cc5c4663a5eea93cac22b921e4c1eed4
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-08 09:13:23 +0200

    TestControllerBuilder

---
+++ src/test/set-basic-aspnet-mvc.test/Builders/TestControllerBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace set_basic_aspnet_mvc.test.Builders
+{
+    public class TestControllerBuilder
+    {
+        //public HttpContextBase HttpContext { get; protected internal set; }
+    }
+}
