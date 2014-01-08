36d192fdb31dd9b872fe2089ef4a8577192c647f
Cihan Coşkun <cihancoskun@gmail.com>
2014-01-08 19:54:44 +0200

    added SearchControllerBuilder

---
+++ src/test/set-basic-aspnet-mvc.test/Builders/SearchControllerBuilder.cs
@@ -0,0 +1,27 @@
+﻿using set_basic_aspnet_mvc.Controllers;
+using set_basic_aspnet_mvc.Domain.Services;
+using set_basic_aspnet_mvc.test.Controllers;
+
+namespace set_basic_aspnet_mvc.test.Builders
+{
+    public class SearchControllerBuilder
+    { 
+        private ISearchService _searchService;
+         
+        public SearchControllerBuilder()
+        { 
+            _searchService = null;
+        }
+          
+        internal SearchControllerBuilder WithSearchService(ISearchService searchService)
+        {
+            _searchService = searchService;
+            return this;
+        }
+
+        internal SearchController Build()
+        {
+            return new SearchController(_searchService);
+        }
+    }
+}
