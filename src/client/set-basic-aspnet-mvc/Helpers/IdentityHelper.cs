d3fc84911b1c728c2af31be74cec511da89fd504
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-08 18:00:03 +0200

    admin contoller role check

--- src/client/set-basic-aspnet-mvc/Helpers/IdentityHelper.cs
+++ src/client/set-basic-aspnet-mvc/Helpers/IdentityHelper.cs
@@ -5,9 +5,9 @@ namespace set_basic_aspnet_mvc.Helpers
 {
     public static class IdentityHelper
     {
-        public static int GetUserId(this IIdentity identity)
+        public static long GetUserId(this IIdentity identity)
         {
-            return identity.IsAuthenticated ? Convert.ToInt32(identity.Name.Split('|')[0]) : 0;
+            return identity.IsAuthenticated ? Convert.ToInt64(identity.Name.Split('|')[0]) : 0;
         }
 
         public static string GetUserFullName(this IIdentity identity)
@@ -18,6 +18,11 @@ namespace set_basic_aspnet_mvc.Helpers
         public static string GetUserEmail(this IIdentity identity)
         {
             return identity.IsAuthenticated ? identity.Name.Split('|')[2] : string.Empty;
+        }
+
+        public static int GetUserRoleId(this IIdentity identity)
+        {
+            return identity.IsAuthenticated ? Convert.ToInt32(identity.Name.Split('|')[3]) : 0;
         }
     }
 }
