26b9ae101e93413a15ff64cd86555ee60cef7561
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-08 15:11:05 +0200

    aws package added

---
+++ src/client/set-basic-aspnet-mvc/Domain/Services/MessageService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace set_basic_aspnet_mvc.Domain.Services
+{
+    public interface IMessageService
+    {
+        Task<bool> SendEmail(string to, string subject, string htmlBody);
+    }
+
+    public class MessageService : IMessageService
+    {
+        public Task<bool> SendEmail(string to, string subject, string htmlBody)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
