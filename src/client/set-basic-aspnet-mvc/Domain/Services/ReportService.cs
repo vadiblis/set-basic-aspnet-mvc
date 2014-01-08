1568380de285228969056baebdbee0ad09c5e0df
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-08 18:03:34 +0200

    search service interface

--- src/client/set-basic-aspnet-mvc/Domain/Services/ReportService.cs
+++ src/client/set-basic-aspnet-mvc/Domain/Services/ReportService.cs
@@ -1,16 +1,28 @@
-﻿using System;
+﻿using set_basic_aspnet_mvc.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace set_basic_aspnet_mvc.Domain.Services
 {
     public interface IReportService
     {
-        
+        Task<List<SearchResult>> GetMostSearchedItems(int count);
+        Task<int> GetTotalUserCount(int count);
     }
 
     public class ReportService : IReportService
     {
+        public Task<List<SearchResult>> GetMostSearchedItems(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetTotalUserCount(int count)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
