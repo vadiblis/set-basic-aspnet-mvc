fdfe259306a2c6d8d536a39d5b1609783aac90ab
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-08 18:07:47 +0200

    report service summarized

--- src/client/set-basic-aspnet-mvc/Domain/Services/ReportService.cs
+++ src/client/set-basic-aspnet-mvc/Domain/Services/ReportService.cs
@@ -1,25 +1,18 @@
-﻿using set_basic_aspnet_mvc.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Threading.Tasks;
-using System.Web;
+
+using set_basic_aspnet_mvc.Domain.Entities;
+using set_basic_aspnet_mvc.Domain.Repositories;
 
 namespace set_basic_aspnet_mvc.Domain.Services
 {
     public interface IReportService
     {
-        Task<List<SearchResult>> GetMostSearchedItems(int count);
         Task<int> GetTotalUserCount(int count);
     }
 
     public class ReportService : IReportService
     {
-        public Task<List<SearchResult>> GetMostSearchedItems(int count)
-        {
-            throw new NotImplementedException();
-        }
-
         public Task<int> GetTotalUserCount(int count)
         {
             throw new NotImplementedException();
