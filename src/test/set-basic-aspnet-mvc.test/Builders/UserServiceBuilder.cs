4d58863abef63679efeb3613b77f0352f05f4565
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-01-12 16:28:36 +0200

    UserServiceBuilder added

---
+++ src/test/set-basic-aspnet-mvc.test/Builders/UserServiceBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+
+using set_basic_aspnet_mvc.Domain.Entities;
+using set_basic_aspnet_mvc.Domain.Repositories;
+using set_basic_aspnet_mvc.Domain.Services;
+
+namespace set_basic_aspnet_mvc.test.Builders
+{
+    public class UserServiceBuilder
+    {
+        private IRepository<User> _userRepository;
+
+        public UserServiceBuilder()
+        {
+            _userRepository = new Mock<IRepository<User>>().Object;
+        }
+
+        internal UserServiceBuilder WithUserRespository(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+            return this;
+        }
+
+        internal UserService Build()
+        {
+            return new UserService(_userRepository);
+        }
+    }
+}
